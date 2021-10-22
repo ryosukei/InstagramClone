9c3b3e1a5b8b28d425fe4d8fc90d56d9a802246d
ryosukei <87470031+ryosukei@users.noreply.github.com>
2021-10-22 09:07:19 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 青野　凌介 on 2021/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
